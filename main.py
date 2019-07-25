546ec82a7497c686a0d2ffc3b82a11dcf4d1b712
Diane P <busybusyus@gmail.com>
2019-07-25 17:44:05 -0500

    finished

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from flask import Flask, request, redirect, render_template
 from flask_sqlalchemy import SQLAlchemy
+from main import db, Blog, User
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:MyNewPass@localhost:8889/build-a-blog'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://blogz:MyNewPass@localhost:8889/blogz'
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
 
@@ -13,6 +14,14 @@ class Blog(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     blog_title = db.Column(db.String(120))
     blog_post = db.Column(db.String(1000))
+    owner_id=db.column(db.Integer, db.foreign_key (user_id))
+
+class user(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id=db.column(db.Integer, db.foreign_key (user_id))
+    password=db.column(db.String(50))
+
 
     def __init__(self, blog_title, blog_post):
         self.blog_title = blog_title
@@ -44,6 +53,13 @@ def index():
 def addPost():
 
     return render_template('addPost.html')
+
+#@app.route('/signup', methods=['POST', 'GET'])
+#@app.route('/login', methods=['POST', 'GET'])
+#@app.route('/index', methods=['POST', 'GET'])
+#@app.route('/logout', methods=['POST'])
+#def Logout():    
+#    return redirect('/?blog')
 
 if __name__ == '__main__':
     app.run()
